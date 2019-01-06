7a8ea5b7646e82822510e48844c73fda11b7feea
Бауыржан <@->
2019-01-07 01:23:45 +0600

    Добавьте файлы проекта.

---
+++ C_Sharp_Homework/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число A = ");
+            int A = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите число B = ");
+            int B = int.Parse(Console.ReadLine());
+
+
+            while (A <= B)
+            {
+                for (int j = 0; j < A; j++)
+                {
+                    Console.Write(A);
+                }
+                A++;
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
